383fefb2beb3a06063d4f15e0630dbdaca41ab92
AAKARSH <akk2080@gmail.com>
2024-08-03 20:17:35 +0530

    Project is initialized using spring initializer.

---
+++ backend/MovieReviewAPI/src/test/java/com/SimpleMovieReviewApi/MovieReviewAPI/MovieReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SimpleMovieReviewApi.MovieReviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
