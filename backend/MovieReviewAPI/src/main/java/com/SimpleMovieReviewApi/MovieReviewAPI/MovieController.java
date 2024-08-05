f36e2526f397be22102b7820fe277efa1a0cf735
AAKARSH <akk2080@gmail.com>
2024-08-05 18:26:38 +0530

    get movie by id added

--- backend/MovieReviewAPI/src/main/java/com/SimpleMovieReviewApi/MovieReviewAPI/MovieController.java
+++ backend/MovieReviewAPI/src/main/java/com/SimpleMovieReviewApi/MovieReviewAPI/MovieController.java
@@ -1,12 +1,15 @@
 package com.SimpleMovieReviewApi.MovieReviewAPI;
 
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -21,6 +24,20 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.getAll(), HttpStatus.OK);
+    }
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id){
+//
+//        return new ResponseEntity<Optional<Movie>>(movieService.getById(id), HttpStatus.OK);
+//
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable String id){
+
+        return new ResponseEntity<Optional<Movie>>(movieService.getByImdbId(id), HttpStatus.OK);
+
     }
 
 }
--- backend/MovieReviewAPI/src/main/java/com/SimpleMovieReviewApi/MovieReviewAPI/MovieService.java
+++ backend/MovieReviewAPI/src/main/java/com/SimpleMovieReviewApi/MovieReviewAPI/MovieService.java
@@ -1,8 +1,10 @@
 package com.SimpleMovieReviewApi.MovieReviewAPI;
 
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +17,13 @@ public class MovieService {
 
     public List<Movie> getAll(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getById(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> getByImdbId(String id) {
+        return movieRepository.findMovieByImdbId(id);
     }
 }
